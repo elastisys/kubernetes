4cc2ded0f2f5213f0510a12879d9d7a1ee2cc286
Cristian Klein <cristian@kleinlabs.eu>
2020-02-13 21:25:48 +0100

    Add demo script

---
+++ demo-kubectl-pkcs11.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+. ./demo-magic.sh
+
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+# TYPE_SPEED=20
+
+# hide the evidence
+clear
+
+#pe "minikube delete"
+#
+#pe "minikube start"
+
+kubectl config unset users.minikube > /dev/null
+kubectl config set-credentials minikube \
+    --client-certificate=$HOME/.minikube/client.crt \
+    --client-key=$HOME/.minikube/client.key > /dev/null
+
+pe "kubectl get pods -n kube-system"
+
+pe "cat ~/.kube/config"
+
+pe "cat ~/.minikube/client.key"
+
+pe "ykman piv import-key 9c ~/.minikube/client.key"
+pe "ykman piv import-certificate 9c ~/.minikube/client.crt"
+
+pe "kubectl config unset users.minikube"
+pe "cat ~/.kube/config"
+
+pe "kubectl config set-credentials minikube --auth-provider=pkcs11 --auth-provider-arg=path=/usr/local/lib/libykcs11.so,pin=123456,slot-id=0,object-id=2"
+pe "cat ~/.kube/config"
+pe "kubectl get pods -n kube-system"
