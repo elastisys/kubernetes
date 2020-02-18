9928397d873a8fa74d2aa2ceec2264add9e1dc7f
Cristian Klein <cristian@kleinlabs.eu>
2020-02-18 14:55:19 +0100

    Wipe client keys in demo

--- demo-kubectl-pkcs11.sh
+++ demo-kubectl-pkcs11.sh
@@ -21,6 +21,19 @@ clear
 #pe "minikube delete"
 #
 #pe "minikube start"
+#
+
+# Save keys produced by minikube
+if [ -e $HOME/.minikube/client.key ]; then
+    cp $HOME/.minikube/client.key $HOME/.minikube/orig-client.key
+fi
+if [ -e $HOME/.minikube/client.crt ]; then
+    cp $HOME/.minikube/client.crt $HOME/.minikube/orig-client.crt
+fi
+
+# Restore client key and certificate
+cp $HOME/.minikube/orig-client.crt $HOME/.minikube/client.crt
+cp $HOME/.minikube/orig-client.key $HOME/.minikube/client.key
 
 kubectl config unset users.minikube > /dev/null
 kubectl config set-credentials minikube \
@@ -35,10 +48,10 @@ pe "cat ~/.minikube/client.key"
 
 pe "ykman piv import-key 9c ~/.minikube/client.key"
 pe "ykman piv import-certificate 9c ~/.minikube/client.crt"
+pe "wipe -f ~/.minikube/client.*"
+pe "kubectl get pods -n kube-system"
 
 pe "kubectl config unset users.minikube"
-pe "cat ~/.kube/config"
-
 pe "kubectl config set-credentials minikube --auth-provider=pkcs11 --auth-provider-arg=path=/usr/local/lib/libykcs11.so,pin=123456,slot-id=0,object-id=2"
 pe "cat ~/.kube/config"
 pe "kubectl get pods -n kube-system"
